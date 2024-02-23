5120470db0bfb875ddaa2d3c152a401bf885ec24
david <ti_4222010306@utcam.edu.mx>
2024-02-23 14:18:13 -0600

    aqui mi trabajo profe: david narvaez felix

--- app/src/main/java/com/david/recyclerview1/MainActivity.kt
+++ app/src/main/java/com/david/recyclerview1/MainActivity.kt
@@ -14,69 +14,70 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ListaDatos.add(
-            dtproductos("2", "harina", "150", "130")
-        )
-        ListaDatos.add(
-            dtproductos("3", "galletas", "140", "110")
-        )
-        ListaDatos.add(
-            dtproductos("4", "jabon", "180", "120")
-        )
-        ListaDatos.add(
-            dtproductos("5", "jamon", "100", "90")
-        )
-        ListaDatos.add(
-            dtproductos("6", "sopa instantanea", "200", "170")
-        )
-        ListaDatos.add(
-            dtproductos("7", "frijol", "30", "20")
-        )
-        ListaDatos.add(
-            dtproductos("8", "arroz", "30", "10")
-        )
-        ListaDatos.add(
-            dtproductos("9", "refresco", "50", "40")
-        )
-        ListaDatos.add(
-            dtproductos("10", "cafe", "19", "15")
-        )
-        ListaDatos.add(
-            dtproductos("11", "azucar", "25", "20")
-        )
-        ListaDatos.add(
-            dtproductos("12", "sabritas", "18", "12")
-        )
-        ListaDatos.add(
-            dtproductos("13", "sal", "18", "14")
-        )
-        ListaDatos.add(
-            dtproductos("14", "cloro", "17", "15")
-        )
-        ListaDatos.add(
-            dtproductos("15", "papel higienico", "25", "20")
-        )
-        ListaDatos.add(
-            dtproductos("16", "queso amarillo", "18", "16")
-        )
-        ListaDatos.add(
-            dtproductos("17","tortilla","26","22")
-        )
-        ListaDatos.add(
-            dtproductos("18","pañales","150","120")
-        )
-        ListaDatos.add(
-            dtproductos("19","toallas femeninas","20","18")
-        )
-        ListaDatos.add(
-            dtproductos("20","cerveza","120","110")
-        )
-        ListaDatos.add(
-            dtproductos("21","desodorante","45","40")
-        )
+
         var tabla =findViewById<RecyclerView>(R.id.Datos)
         recycler=tabla
         recycler.layoutManager=LinearLayoutManager(this)
         recycler.adapter=productosAdapter(this, ListaDatos)
+        ListaDatos.add(
+            dtproductos("id del producto:2", "producto:harina", "precio por menudeo:150", "precio por mayoreo:130")
+        )
+        ListaDatos.add(
+            dtproductos("id:3", "producto: galletas", "precio menudeo: 140", "precio mayoreo: 110")
+        )
+        ListaDatos.add(
+            dtproductos("id: 4", "producto :jabon", "precio menudeo: 180", "precio mayoreo: 120")
+        )
+        ListaDatos.add(
+            dtproductos("id: 5", "producto: jamon", "precio menudeo: 100", "precio mayoreo: 90")
+        )
+        ListaDatos.add(
+            dtproductos("id: 6", "producto: sopa instantanea", "precio menudeo: 200", "precio mayoreo: 170")
+        )
+        ListaDatos.add(
+            dtproductos("id: 7", "producto: frijol", "precio menudeo :30", "precio mayoreo: 20")
+        )
+        ListaDatos.add(
+            dtproductos("id: 8", "producto: arroz", "precio  menudeo: 30", "precio mayoreo: 10")
+        )
+        ListaDatos.add(
+            dtproductos("id: 9", "producto: refresco", "precio menudoe: 50", "precio mayoreo: 40")
+        )
+        ListaDatos.add(
+            dtproductos("id: 10", "producto: cafe", "precio menudeo: 19", "precio mayoreo: 15")
+        )
+        ListaDatos.add(
+            dtproductos("id: 11", "producto: azucar", "precio menudeo: 25", "precio mayoreo: 20")
+        )
+        ListaDatos.add(
+            dtproductos("id: 12", "producto: sabritas", "precio menudeo: 18", "precio mayoreo:12")
+        )
+        ListaDatos.add(
+            dtproductos("id: 13", "producto: sal", "precio menudeo: 18", "precio mayoreo: 14")
+        )
+        ListaDatos.add(
+            dtproductos("id: 14", "producto: cloro", "precio menudeo: 17", "precio mayoreo: 15")
+        )
+        ListaDatos.add(
+            dtproductos("id: 15", "producto: papel higienico", "precio menudeo: 25", "precio mayoreo: 20")
+        )
+        ListaDatos.add(
+            dtproductos("id: 16", "producto: queso amarillo", "precio menudeo: 18", "precio mayoreo: 16")
+        )
+        ListaDatos.add(
+            dtproductos("id: 17", "producto: tortilla", "precio menudeo: 26", "precio mayoreo: 22")
+        )
+        ListaDatos.add(
+            dtproductos("id: 18", "producto: pañales", "precio menudeo: 150", "precio mayoreo: 120")
+        )
+        ListaDatos.add(
+            dtproductos("id: 19", "producto: toallas femeninas", "precio menudeo: 20", "precio mayoreo: 18")
+        )
+        ListaDatos.add(
+            dtproductos("id: 20", "producto: cerveza", "precio menudeo: 120", "precio mayoreo: 110")
+        )
+        ListaDatos.add(
+            dtproductos("id: 21", "producto: desodorante", "precio menudeo: 45", "precio mayoreo: 40")
+        )
     }
 }
